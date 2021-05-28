b427a2a7be57e898efb64ca9f34b00764ae9bf3a
ColinSORourke <csorourk@ucsc.edu>
2021-05-27 18:33:38 -0700

    Better Arrow logic

--- src/prefab/Player.js
+++ src/prefab/Player.js
@@ -45,7 +45,6 @@ class Player extends Phaser.GameObjects.Sprite {
         this.initializeAnims();
 
         this.arrow = scene.add.sprite(this.x, this.y - 40, 'arrow');
-        console.log(this.arrow);
         this.arrow.visible = false;
     }
 
@@ -90,7 +89,9 @@ class Player extends Phaser.GameObjects.Sprite {
     }
 
     update() {
-        console.log(this.arrow);
+        if (this.body.touching.none && this.arrow.visible){
+            this.arrow.visible = false;
+        }
         this.arrow.x = this.x;
         this.arrow.y = this.y - 40;
         this.animFSM.step();
@@ -111,15 +112,13 @@ class Player extends Phaser.GameObjects.Sprite {
                 this.body.setDragX(0);
                 this.body.setAccelerationX(-this.ACCELERATION);
                 this.setFlip(true, false);         
-                this.arrow.visible = false;
             } else if(cursors.right.isDown) {
                 if (this.body.velocity.x < 0){
                     this.body.setVelocityX(0);
                 }
                 this.body.setDragX(0);
                 this.body.setAccelerationX(this.ACCELERATION);
-                this.resetFlip();       
-                this.arrow.visible = false;   
+                this.resetFlip();        
             } else {
                 // set acceleration to 0 so DRAG will take over
                 this.body.setAccelerationX(0);
