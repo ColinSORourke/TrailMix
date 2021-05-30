64590ad4d18819fdc5b4ec40960f0bc0a7eae271
Dennis Zabluda <dennisz125.public@gmail.com>
2021-05-29 17:19:40 -0700

    Sign Update

--- src/prefab/SignObj.js
+++ src/prefab/SignObj.js
@@ -10,8 +10,14 @@ class SignObj extends Phaser.GameObjects.Sprite {
         scene.physics.add.overlap(player, this, function(){
             player.arrow.visible = true;
             if (Phaser.Input.Keyboard.JustDown(cursors.up)){
+                let graphics = scene.add.graphics();
+                // Add Background
+                let SBGW = 350, SBGH = 200;
+                let signBackground = graphics.fillStyle(0x964B00, 1).fillRoundedRect(game.config.width/2 - SBGW/2, game.config.height/2 - SBGH/2, SBGW, SBGH, 8).setScrollFactor(0).setName('Sign');
+                // Add text and it's warping property
                 let myText = scene.add.bitmapText(game.config.width/2, game.config.height/2, 'gem', text, 20).setOrigin(0.5).setScrollFactor(0,0);
                 myText.maxWidth = game.config.width / 3;
+                
                 
                 scene.tweens.add({
                     targets: myText,
