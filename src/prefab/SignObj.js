33693cc828749d12d5fbf4b66273e6f70288471a
Dennis Zabluda <dennisz125.public@gmail.com>
2021-05-29 17:34:00 -0700

    Dynamically sign update

--- src/prefab/SignObj.js
+++ src/prefab/SignObj.js
@@ -11,23 +11,30 @@ class SignObj extends Phaser.GameObjects.Sprite {
             player.arrow.visible = true;
             if (Phaser.Input.Keyboard.JustDown(cursors.up)){
                 let graphics = scene.add.graphics();
-                // Add Background
-                let SBGW = 350, SBGH = 200;
-                let signBackground = graphics.fillStyle(0x964B00, 1).fillRoundedRect(game.config.width/2 - SBGW/2, game.config.height/2 - SBGH/2, SBGW, SBGH, 8).setScrollFactor(0).setName('Sign');
+
                 // Add text and it's warping property
                 let myText = scene.add.bitmapText(game.config.width/2, game.config.height/2, 'gem', text, 20).setOrigin(0.5).setScrollFactor(0,0);
                 myText.maxWidth = game.config.width / 3;
-                
-                
+                let bounds = myText.getTextBounds(true);
+                myText.setDepth(10);
+
+                // Add Background
+                let SBGW = bounds.global.width + 50, SBGH = bounds.global.height + 50;
+                let signBackground = graphics.fillStyle(0x964B00, 1).fillRoundedRect(game.config.width/2 - SBGW/2, game.config.height/2 - SBGH/2, SBGW, SBGH, 8).setScrollFactor(0).setName('Sign');
+                signBackground.setDepth(9);
+
+                // Delay Call then 'destory' the sign
+                scene.time.delayedCall(1500, () => {
                 scene.tweens.add({
                     targets: [myText, signBackground],
                     alpha: 0,
-                    duration: 6000,
+                    duration: 3000,
                     ease: 'Linear',
                     onComplete: function() {
                       myText.destroy();
                     }
                   });
+                }, null, this);
             }
         });
     }
