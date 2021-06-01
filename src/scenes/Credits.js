e8f9f422f3710a2386e8827f497a5284f0d47038
Dennis Zabluda <dennisz125.public@gmail.com>
2021-06-01 16:59:20 -0700

    Credits Scene

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -17,16 +17,11 @@ class Credits extends Phaser.Scene {
         this.add.bitmapText(game.config.width/2, game.config.height/2, 'gem', 'Artist:', 40).setOrigin(0.5);
         this.add.bitmapText(game.config.width/2, game.config.height/2 + 50, 'gem', 'Daniel Aughenbaugh', 28).setOrigin(0.5);
 
-<<<<<<< HEAD
-        // Menu 
-        let MenuButton = this.add.bitmapText(game.config.width/2, game.config.height/2 + 150, 'gem', 'Back', 40).setOrigin(0.5);
-=======
         this.add.bitmapText(game.config.width/2, game.config.height/2 + 150, 'gem', 'Other Contributions:', 40).setOrigin(0.5);
         this.add.bitmapText(game.config.width/2, game.config.height/2 + 200, 'gem', 'Ambient Forest and Wind Sounds by @BurghRecords on YouTube', 28).setOrigin(0.5);
 
         // Menu
         let MenuButton = this.add.bitmapText(game.config.width/2, game.config.height/2 + 300, 'gem', 'Back', 40).setOrigin(0.5);
->>>>>>> de24c3612172cb3c8683c7e0497ff0b1a0e9300d
 
         // Menu Button
         MenuButton.setInteractive();
