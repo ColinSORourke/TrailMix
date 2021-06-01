1cbf95f2b514578031157e8a66cd9394dbd25b77
Dennis Zabluda <dennisz125.public@gmail.com>
2021-06-01 13:01:25 -0700

    Update Credits Scene

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -7,40 +7,18 @@ class Credits extends Phaser.Scene {
     }
 
     create() {
-        let creditConfig = {
-            fontFamily: 'Garamond',
-            fontSize: '28px',
-            color: '#FFFFF0',
-            alighn: 'right',
-            padding: {
-                top: 5,
-                bottom: 5
-            },
-            fixedWidth: 0
-        }
-  
         // Title
-        creditConfig.fontSize = "48px";    
-        this.add.text(game.config.width/2, game.config.height/3 - 150, 'CREDITS:', creditConfig).setOrigin(0.5);
+        this.add.bitmapText(game.config.width/2, game.config.height/3 - 150, 'gem', 'CREDITS:', 48).setOrigin(0.5);
         
-        creditConfig.fontSize = "40px";    
-        this.add.text(game.config.width/2, game.config.height/3 - 50, 'Programmers:', creditConfig).setOrigin(0.5);
+        this.add.bitmapText(game.config.width/2, game.config.height/3 - 50, 'gem', 'Programmers:', 40).setOrigin(0.5);
+        this.add.bitmapText(game.config.width/2, game.config.height/3, 'gem', 'Colin O\'Rourke', 28).setOrigin(0.5);
+        this.add.bitmapText(game.config.width/2, game.config.height/3 + 50, 'gem', 'Dennis Zabluda', 28).setOrigin(0.5);
 
-        creditConfig.fontSize = "28px";    
-        this.add.text(game.config.width/2, game.config.height/3, 'Colin O\'Rourke', creditConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/3 + 50, 'Dennis Zabluda', creditConfig).setOrigin(0.5);
-
-        creditConfig.fontSize = "40px";    
-        this.add.text(game.config.width/2, game.config.height/2, 'Artist:', creditConfig).setOrigin(0.5);
-
-        creditConfig.fontSize = "28px"; 
-        this.add.text(game.config.width/2, game.config.height/2 + 50, 'Daniel Aughenbaugh', creditConfig).setOrigin(0.5);
-
-        creditConfig.fontSize = "40px";   
-        creditConfig.backgroundColor = '#04471C';
+        this.add.bitmapText(game.config.width/2, game.config.height/2, 'gem', 'Artist:', 40).setOrigin(0.5);
+        this.add.bitmapText(game.config.width/2, game.config.height/2 + 50, 'gem', 'Daniel Aughenbaugh', 28).setOrigin(0.5);
 
         // Menu
-        let MenuButton = this.add.text(game.config.width/2, game.config.height/2 + 150, 'Back', creditConfig).setOrigin(0.5);
+        let MenuButton = this.add.bitmapText(game.config.width/2, game.config.height/2 + 150, 'gem', 'Back', 40).setOrigin(0.5);
 
         // Menu Button
         MenuButton.setInteractive();
